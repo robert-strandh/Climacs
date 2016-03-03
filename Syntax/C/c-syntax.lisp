3834be4670292b05f7cf03fe194ebba0cb2771b3
Robert Strandh <robert.strandh@gmail.com>
2016-03-03 10:38:04 +0100

    Improve comment stating the purpose of the component.

--- Syntax/C/c-syntax.lisp
+++ Syntax/C/c-syntax.lisp
@@ -22,7 +22,7 @@
 ;;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;;; Boston, MA  02111-1307  USA.
 
-;;; Syntax module for analysing C
+;;; Syntax module for analyzing C source code.
 
 (in-package :climacs-c-syntax)
 
